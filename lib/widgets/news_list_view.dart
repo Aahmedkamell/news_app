6621e2c7d85e3f8d10ef84b030af698a9b6fc35c
Aahmedkamell <aahmed5kamell@gmail.com>
2025-09-08 16:29:47 +0300

    Fix NewsListView to properly initialize NewsService for fetching general news

--- lib/widgets/news_list_view.dart
+++ lib/widgets/news_list_view.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app_ui_setup/services/news_service.dart';
 import 'package:news_app_ui_setup/widgets/news_article.dart';
 
 class NewsListView extends StatelessWidget {
@@ -8,6 +10,8 @@ class NewsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    NewsService(Dio()).getGeneralNews;
     return SliverList(
         delegate: SliverChildBuilderDelegate(childCount: 10, (context, index) {
       return Padding(
