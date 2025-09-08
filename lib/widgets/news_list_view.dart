04dc77e666b6af6658c0f6dff55329a0537d4c7a
Aahmedkamell <aahmed5kamell@gmail.com>
2025-09-08 19:16:27 +0300

    Refactor NewsListView to use StatefulWidget and initialize state for future enhancements

--- lib/widgets/news_list_view.dart
+++ lib/widgets/news_list_view.dart
@@ -3,19 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:news_app_ui_setup/services/news_service.dart';
 import 'package:news_app_ui_setup/widgets/news_article.dart';
 
-class NewsListView extends StatelessWidget {
+class NewsListView extends StatefulWidget {
   const NewsListView({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  State<NewsListView> createState() => _NewsListViewState();
+}
 
+class _NewsListViewState extends State<NewsListView> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
     NewsService(Dio()).getGeneralNews;
     return SliverList(
         delegate: SliverChildBuilderDelegate(childCount: 10, (context, index) {
       return Padding(
-        padding: const EdgeInsets.only(bottom:22),
+        padding: const EdgeInsets.only(bottom: 22),
         child: NewsArticle(),
       );
     }));
