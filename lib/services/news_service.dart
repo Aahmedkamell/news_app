ddab6f8ca6619a2977498ae040327769ee5df5e4
Aahmedkamell <aahmed5kamell@gmail.com>
2025-09-25 16:08:12 +0300

    Refactor NewsListViewBuilder to use FutureBuilder for improved data fetching and error handling; update loading indicator and error message display.

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -7,22 +7,27 @@ class NewsService {
   NewsService(this.dio);
 
   Future<List<ArticleModel>> getGeneralNews() async {
-    var response = await dio.get(
-        'https://newsapi.org/v2/top-headlines?apiKey=7e2130d74f1f4d71afb7106d05663343&category=general');
-    Map<String, dynamic> jsonData = response.data;
+    try {
+      var response = await dio.get(
+          'https://newsapi.org/v2/top-headlines?apiKey=7e2130d74f1f4d71afb7106d05663343&category=general');
+      Map<String, dynamic> jsonData = response.data;
 
-    List<dynamic> articles = jsonData['articles'];
+      List<dynamic> articles = jsonData['articles'];
 
-    List<ArticleModel> articleList = [];
+      List<ArticleModel> articleList = [];
 
-    for (var article in articles) {
-      ArticleModel articleModel = ArticleModel(
-          image: article['urlToImage'],
-          title: article['title'],
-          subTitle: article['description']);
+      for (var article in articles) {
+        ArticleModel articleModel = ArticleModel(
+            image: article['urlToImage'],
+            title: article['title'],
+            subTitle: article['description']);
 
-      articleList.add(articleModel);
+        articleList.add(articleModel);
+      }
+      return articleList;
+    } on Exception catch (e) {
+      throw Exception('Failed to load news');
+      // TODO
     }
-    return articleList;
   }
 }
--- lib/widgets/news_list_builder.dart
+++ lib/widgets/news_list_builder.dart
@@ -1,49 +1,56 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:news_app_ui_setup/models/article_model.dart';
 import 'package:news_app_ui_setup/services/news_service.dart';
 import 'package:news_app_ui_setup/widgets/news_list_view.dart';
 
-class NewsListViewBuilder extends StatefulWidget {
-  NewsListViewBuilder({
+class NewsListViewBuilder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: NewsService(Dio()).getGeneralNews(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return NewsListView(articles: snapshot.data!);
+          } else if (snapshot.hasError) {
+            return const SliverToBoxAdapter(
+              child: Center(
+                child: ErrorMessage(),
+              ),
+            );
+          } else {
+            return SliverFillRemaining(
+              hasScrollBody: false,
+              child: Center(
+                child: LoadingIndicator(),
+              ),
+            );
+          }
+        });
+  }
+}
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({
     super.key,
   });
 
   @override
-  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
+  Widget build(BuildContext context) {
+    return CircularProgressIndicator(
+      color: Colors.deepOrange,
+    );
+  }
 }
 
-class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
-  List<ArticleModel> articles = [];
 
-  bool isLoading = true;
-  @override
-  void initState() {
-    super.initState();
 
-    getGeneralNews();
-  }
-
-  Future<void> getGeneralNews() async {
-    articles = await NewsService(Dio()).getGeneralNews();
-    setState(() {
-      isLoading = false;
-    });
-  }
+class ErrorMessage extends StatelessWidget {
+  const ErrorMessage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? SliverFillRemaining(
-            hasScrollBody: false,
-            child: Center(
-              child: CircularProgressIndicator(
-                color: Colors.deepOrange,
-              ),
-            ),
-          )
-        : NewsListView(
-            articles: articles,
-          );
+    return Text('opps! there was an error');
   }
 }
