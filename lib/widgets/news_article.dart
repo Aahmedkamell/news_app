8ab91aede75c01219d606f61e1abcc8125777f5e
Aahmedkamell <aahmed5kamell@gmail.com>
2025-09-30 19:59:58 +0300

    Refactor NewsArticle widget to use CachedNetworkImage for improved image loading and error handling; update pubspec.yaml to include cached_network_image dependency.

--- lib/widgets/news_article.dart
+++ lib/widgets/news_article.dart
@@ -1,10 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:news_app_ui_setup/models/article_model.dart';
 
 class NewsArticle extends StatelessWidget {
   const NewsArticle({super.key, required this.article});
-final ArticleModel article;
+  final ArticleModel article;
   @override
   Widget build(BuildContext context) {
     return Directionality(
@@ -14,8 +15,54 @@ final ArticleModel article;
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ClipRRect(
-            borderRadius: BorderRadius.circular(6),
-            child: Image.network(
+              borderRadius: BorderRadius.circular(6),
+              child: CachedNetworkImage(
+                  imageUrl: article.image ?? 'https://via.placeholder.com/150',
+                  height: 200,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                  fadeInDuration: const Duration(milliseconds: 500),
+                  fadeOutDuration: const Duration(milliseconds: 500),
+                  placeholder: (context, url) => Container(
+                        height: 200,
+                        color: Colors.grey[200],
+                        child: const Center(child: CircularProgressIndicator()),
+                      ),
+                  errorWidget: (context, url, error) => Container(
+                      height: 200,
+                      color: Colors.grey[200],
+                      child: const Center(
+                        child: Icon(Icons.error),
+                      )))),
+          const SizedBox(height: 12),
+          Text(
+            article.title,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+            style: GoogleFonts.tajawal(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Colors.black87,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            article.subTitle ?? '',
+            maxLines: 2,
+            style: GoogleFonts.tajawal(
+              color: Colors.grey,
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+/*
+Image.network(
               article.image??'https://via.placeholder.com/150',
               height: 200,
               width: double.infinity,
@@ -42,30 +89,4 @@ final ArticleModel article;
                   child: const Icon(Icons.error),
                 );
               },
-            ),
-          ),
-          const SizedBox(height: 12),
-          Text(
-            article.title,
-            overflow: TextOverflow.ellipsis,
-            maxLines: 2,
-            style: GoogleFonts.tajawal(
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
-              color: Colors.black87,
-            ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            article.subTitle??'',
-            maxLines: 2,
-            style: GoogleFonts.tajawal(
-              color: Colors.grey,
-              fontSize: 14,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+            ),*/
